06fb376be8a30c25c1e9cb594ae48d7ce0b8a9c5
llyymm23 <jsy98bam1023@naver.com>
2024-01-30 11:33:58 +0900

    raw-query 테이블 목록 조회, 데이터 삽입

--- app.js
+++ app.js
@@ -19,8 +19,28 @@ app.post('/api/tables', async (req, res, next) => {
         createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
     )
     `);
-    return res.status(201).json({ messaage: "테이블 생성에 성공하였습니다." });
-})
+    return res.status(201).json({ messaage : '테이블 생성에 성공하였습니다.' });
+});
+
+app.get('/api/tables',async(req,res,next)=>{
+    const [tableList] = await connect.promise().query(`
+    SHOW TABLES
+    `)
+    const tableName = tableList.map(table => Object.values(table)[0]);
+    return res.status(200).json({message : tableName});
+});
+
+app.post('/api/tables/:tableName/items', async(req,res,next) => {
+    const {tableName} = req.params;
+    const {name} = req.body;
+
+    await connect.promise().query(`
+    INSERT INTO ${tableName} (name)
+    VALUES ('${name}')
+    `);
+
+    return res.status(201).json({message : '데이터 생성에 성공하였습니다.'});
+});
 
 app.listen(PORT, () => {
     console.log(PORT, '포트로 서버가 열렸어요!');
